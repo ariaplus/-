1ecc2df70e4c90ff0c18abd75d9316fe4e69317e
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-02-21 21:23:47 +1200

    Update foryou-button.tsx

--- src/components/explore/foryou-button.tsx
+++ src/components/explore/foryou-button.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-
+import Link from 'next/link'
 import { useState, useEffect } from 'react';
 import { toast } from 'react-hot-toast';
 import { checkUsernameAvailability, updateUsername } from '@lib/firebase/utils';
