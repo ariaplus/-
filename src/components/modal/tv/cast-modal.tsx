f74865683ab17f02eae25d307e23195f355aece0
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-02-21 20:02:44 +1200

    Create cast-modal.tsx

---
+++ src/components/modal/tv/cast-modal.tsx
@@ -0,0 +1,83 @@
+import { Dialog } from '@headlessui/react';
+import { CustomIcon } from '@components/ui/custom-icon';
+import { Button } from '@components/ui/button';
+import type { ReactNode, FormEvent } from 'react';
+import { ArplImage } from '@components/ui/ariaplus';
+
+
+type UsernameModalProps = {
+  loading: boolean;
+  children: ReactNode;
+  available: boolean;
+  alreadySet: boolean;
+  changeUsername: (e: FormEvent<HTMLFormElement>) => Promise<void>;
+  cancelUpdateUsername: () => void;
+};
+
+const usernameModalData = [
+  {
+    title: 'What should we call you?',
+    description: 'Your @username is unique. You can always change it later.',
+    cancelLabel: 'Skip'
+  },
+  {
+    title: 'Aria+ Wide',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce luctus mollis ex a sodales. Quisque molestie massa quis dolor elementum rhoncus. Nullam scelerisque elementum elit, nec vestibulum augue luctus id. Aliquam et dolor non neque pulvinar sodales a nec mi. Morbi varius a erat in fermentum. Donec sed scelerisque purus. Integer sed libero sit amet erat aliquet facilisis in quis augue. Sed sollicitudin egestas fermentum. Integer sit amet tincidunt velit. Maecenas tincidunt justo at erat luctus feugiat. Pellentesque eget diam vitae elit ultrices ullamcorper. Aliquam tincidunt quam elit, ut interdum nisi maximus in. Donec eget enim feugiat, rutrum turpis sit amet, egestas tortor. Aenean lobortis finibus hendrerit.',
+    cancelLabel: 'Cancel'
+  }
+] as const;
+
+export function UsernameModal({
+  loading,
+  children,
+  available,
+  alreadySet,
+  changeUsername,
+  cancelUpdateUsername
+}: UsernameModalProps): JSX.Element {
+  const { title, description, cancelLabel } = usernameModalData[+alreadySet];
+
+  return (
+    <form
+      className='flex h-full flex-col justify-between'
+      onSubmit={changeUsername}
+    >
+      <div className='flex flex-col gap-6'>
+        <div className='flex flex-col gap-4'>
+          <i className='mx-auto'>
+      <ArplImage
+          imgClassName='arplicon'
+          blurClassName='none'
+          src='/assets/ariaplus.svg'
+          alt='Twitter banner'
+          layout='fill'
+          width='40px'
+          height='40px'
+          useSkeleton
+       />
+          </i>
+          <div className='flex items-center flex-col gap-2'>
+            <Dialog.Title className='text-2xl font-bold xs:text-3xl sm:text-4xl'>
+              {title}
+            </Dialog.Title>
+            <Dialog.Description className='text-light-secondary dark:text-dark-secondary'>
+              {description}
+            </Dialog.Description>
+          </div>
+        </div>
+        {children}
+      </div>
+      <div className='flex flex-col gap-3 inner:py-2 inner:font-bold'>
+
+        <Button
+          className='border border-light-line-reply hover:bg-light-primary/10 focus-visible:bg-light-primary/10
+                     active:bg-light-primary/20 dark:border-light-secondary dark:text-light-border 
+                     dark:hover:bg-light-border/10 dark:focus-visible:bg-light-border/10 
+                     dark:active:bg-light-border/20'
+          onClick={cancelUpdateUsername}
+        >Close</Button>
+      </div>
+    </form>
+  );
+}
