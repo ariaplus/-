38529ea66b6ee2406673d3464647d7fdd812a9f7
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-02-22 02:53:44 +1200

    Update paymentcard.tsx

--- src/components/pay/paymentcard.tsx
+++ src/components/pay/paymentcard.tsx
@@ -11,8 +11,9 @@ const userId = user?.id as string;
       <a className='block bg-cover border-solid border-2 border-white bg-center h-64 rounded-lg shadow-md' style={{ backgroundImage: 'url(/path/to/image.jpg)' }}>
         <div className='p-4'>
           <h2 className='text-xl font-semibold mb-1' aria-label='Aria+ Pay Card'>Aria+ Pay Card</h2>
-           <p className='text-xs text-light-secondary dark:text-dark-secondary'>
+        <p className='text-xs text-light-secondary dark:text-dark-secondary'>
             @{user?.username}
+          </p>
         </div>
       </a>
     </Link>
