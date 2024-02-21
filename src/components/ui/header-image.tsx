a9920f083b4d5f3708e1c26ee4928e2e0dbc578f
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-02-21 19:55:56 +1200

    Add files via upload

---
+++ src/components/ui/header-image.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import Image from 'next/image';
+import cn from 'clsx';
+import type { ReactNode } from 'react';
+import type { ImageProps } from 'next/image';
+
+type NextImageProps = {
+  alt: string;
+  width?: string | number;
+  children?: ReactNode;
+  useSkeleton?: boolean;
+  imgClassName?: string;
+  previewCount?: number;
+  blurClassName?: string;
+} & ImageProps;
+
+/**
+ *
+ * @description Must set width and height, if not add layout='fill'
+ * @param useSkeleton add background with pulse animation, don't use it if image is transparent
+ */
+export function HeaderImage({
+  src,
+  alt,
+  width,
+  height,
+  children,
+  className,
+  useSkeleton,
+  imgClassName,
+  previewCount,
+  blurClassName,
+  ...rest
+}: NextImageProps): JSX.Element {
+  const [loading, setLoading] = useState(!!useSkeleton);
+
+  const handleLoad = (): void => setLoading(false);
+
+  return (
+    <figure style={{ width }} className={className}>
+      <Image
+        className={cn(
+          imgClassName,
+          loading
+            ? blurClassName ??
+                'animate-pulse bg-light-secondary dark:bg-dark-secondary'
+            : previewCount === 1
+            ? '!h-24 !w-24 rounded-lg object-contain relative inset-0'
+            : 'object-cover w-full h-full relative',
+              'transition-opacity duration-300'
+        )}
+        src={src}
+        width={width}
+        height={height}
+        alt={alt}
+        onLoadingComplete={handleLoad}
+        layout='responsive'
+      />
+      {children}
+    </figure>
+  );
+}
