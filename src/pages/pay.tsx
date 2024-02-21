9624ebb8551b2fbd4e1778ab1179263620af4156
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-02-22 02:42:01 +1200

    Update pay.tsx

--- src/pages/pay.tsx
+++ src/pages/pay.tsx
@@ -46,12 +46,7 @@ export default function Bookmarks(): JSX.Element {
        <PaySettingsButton/>
           </div>
       </MainHeader>
-      <PaymentCard
-          src: string;
-           title: string;
-           amount: string;
-          dueDate: string;
-         />
+      <PaymentCard />
     </MainContainer>
   );
 }
