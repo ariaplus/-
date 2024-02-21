f0ba8bb241a75bd602477fd557d198bbec70b9d3
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-02-21 21:16:16 +1200

    Update foryou-button.tsx

--- src/components/explore/foryou-button.tsx
+++ src/components/explore/foryou-button.tsx
@@ -1,27 +1,130 @@
-import Link from 'next/link'
-import type { IconName } from '@components/ui/hero-icon';
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { useState, useEffect } from 'react';
+import { toast } from 'react-hot-toast';
+import { checkUsernameAvailability, updateUsername } from '@lib/firebase/utils';
+import { useAuth } from '@lib/context/auth-context';
+import { useModal } from '@lib/hooks/useModal';
+import { isValidUsername } from '@lib/validation';
+import { sleep } from '@lib/utils';
+import { Button } from '@components/ui/button';
 import { HeroIcon } from '@components/ui/hero-icon';
 import { ToolTip } from '@components/ui/tooltip';
+import { Modal } from '@components/modal/modal';
+import { UsernameModal } from '@components/modal/username-modal';
+import { InputField } from '@components/input/input-field';
+import type { FormEvent, ChangeEvent } from 'react';
 
-interface Props {
-  href: string
-  icon: IconName
-  children: React.ReactNode
-}
+export function ForYouButton(): JSX.Element {
+  const [alreadySet, setAlreadySet] = useState(false);
+  const [available, setAvailable] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [visited, setVisited] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-export const ForYouButton: React.FC<Props> = ({ href, icon, children }) => {
+  const { user } = useAuth();
+  const { open, openModal, closeModal } = useModal();
+  const handleClick = () => {
+    // navigate to the desired page
+    Router.push('/foryou');
+
+  useEffect(() => {
+    const checkAvailability = async (value: string): Promise<void> => {
+      const empty = await checkUsernameAvailability(value);
+
+      if (empty) setAvailable(true);
+      else {
+        setAvailable(false);
+        setErrorMessage('This username has been taken. Please choose another.');
+      }
+    };
+
+    if (!visited && inputValue.length > 0) setVisited(true);
+
+    if (visited) {
+      if (errorMessage) setErrorMessage('');
+
+      const error = isValidUsername(user?.username as string, inputValue);
+
+      if (error) {
+        setAvailable(false);
+        setErrorMessage(error);
+      } else void checkAvailability(inputValue);
+    }
+  }, [inputValue]);
+
+  useEffect(() => {
+    if (!user?.updatedAt) openModal();
+    else setAlreadySet(true);
+  }, []);
+
+  const changeUsername = async (
+    e: FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    e.preventDefault();
+
+    if (!available) return;
+
+    setLoading(true);
+
+    await sleep(500);
+
+    await updateUsername(user?.id as string, inputValue);
+
+    closeModal();
+
+    setLoading(false);
+
+    setInputValue('');
+    setVisited(false);
+    setAvailable(false);
+
+    toast.success('Username updated successfully');
+  };
+
+  const cancelUpdateUsername = (): void => {
+    closeModal();
+    if (!alreadySet) void updateUsername(user?.id as string);
+  };
+
+  const handleChange = ({
+    target: { value }
+  }: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void =>
+    setInputValue(value);
+
   return (
-    <Link href={href}>
-      <a className="dark-bg-tab group relative p-2 hover:bg-light-primary/10
+    <>
+      <Modal
+        modalClassName='flex flex-col gap-6 max-w-xl bg-main-background w-full p-8 rounded-2xl h-[576px]'
+        open={open}
+        closeModal={cancelUpdateUsername}
+      >
+        <UsernameModal
+          loading={loading}
+          available={available}
+          alreadySet={alreadySet}
+          changeUsername={changeUsername}
+          cancelUpdateUsername={cancelUpdateUsername}
+        >
+          <InputField
+            label='Username'
+            inputId='username'
+            inputValue={inputValue}
+            errorMessage={errorMessage}
+            handleChange={handleChange}
+          />
+        </UsernameModal>
+      </Modal>
+      <Button
+        className='dark-bg-tab group relative p-2 hover:bg-light-primary/10
                    active:bg-light-primary/20 dark:hover:bg-dark-primary/10 
-                   dark:active:bg-dark-primary/20">
-        <span className="mr-2">
-          {/* replace with your icon library's component */}
+                   dark:active:bg-dark-primary/20'
+        onClick={handleClick}
+      >
         <HeroIcon className='h-5 w-5' iconName='AtSymbolIcon' />
         <ToolTip tip='Top tweets' />
-        </span>
-        {children}
-      </a>
-    </Link>
-  )
+      </Button>
+    </>
+  );
 }
