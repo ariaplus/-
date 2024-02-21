5963a568b5d3619a26857715314fc53131d0babc
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-02-22 02:51:52 +1200

    Update paymentcard.tsx

--- src/components/pay/paymentcard.tsx
+++ src/components/pay/paymentcard.tsx
@@ -1,15 +1,18 @@
 import Link from 'next/link';
+import { useAuth } from '@lib/context/auth-context';
 
 export function PaymentCard() {
+
+const { user } = useAuth();
+const userId = user?.id as string;
+  
   return (
     <Link href='/pay/card'>
-      <a className='block bg-cover bg-center h-64 rounded-lg shadow-md' style={{ backgroundImage: 'url(/path/to/image.jpg)' }}>
+      <a className='block bg-cover border-solid border-2 border-white bg-center h-64 rounded-lg shadow-md' style={{ backgroundImage: 'url(/path/to/image.jpg)' }}>
         <div className='p-4'>
           <h2 className='text-xl font-semibold mb-1' aria-label='Aria+ Pay Card'>Aria+ Pay Card</h2>
-          <p className='text-gray-700 mb-2'>New Purchase Amount</p>
-          <p className='text-2xl font-bold mb-4'>$415.98</p>
-          <p className='text-gray-700 mb-4'>Upcoming Payments</p>
-          <p className='text-gray-700'>Sep 9, 2023 - $68.16<br/>Sep 12, 2023<br/>Sep 14, 2023 - $20.52</p>
+           <p className='text-xs text-light-secondary dark:text-dark-secondary'>
+            @{user?.username}
         </div>
       </a>
     </Link>
