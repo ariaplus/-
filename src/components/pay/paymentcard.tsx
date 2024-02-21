15d41fad14b343894c3f8ff4d7d1e55c1a149094
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-02-22 03:12:41 +1200

    Update paymentcard.tsx

--- src/components/pay/paymentcard.tsx
+++ src/components/pay/paymentcard.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { useAuth } from '@lib/context/auth-context';
+import { ArplImage } from '@components/ui/ariaplus';
 
 export function PaymentCard() {
 
@@ -8,7 +9,20 @@ const userId = user?.id as string;
   
   return (
     <Link href='/pay/card'>
-      <a className='block bg-cover border-solid border-2 border-white bg-center h-64 rounded-lg shadow-md px-8' style={{ backgroundImage: 'url(/path/to/image.jpg)' }}>
+      <a className='block bg-cover border-solid border-2 border-white bg-center h-64 rounded-lg shadow-md pt-48' style={{ backgroundImage: 'url(/path/to/image.jpg)' }}>
+            <div className='flex flex-col items-center gap-6'>
+        <div className='flex flex-col gap-4'>
+      <ArplImage
+          imgClassName='arplicon'
+          blurClassName='none'
+          src='/assets/ariaplus.svg'
+          alt='Twitter banner'
+          layout='fill'
+          width='40px'
+          height='40px'
+          useSkeleton
+       />
+            </div>
         <div className='p-4 items:end content-end'>
           <h2 className='text-xl font-semibold mb-1' aria-label='Aria+ Pay Card'>Aria+ Pay Card</h2>
         <p className='text-xs text-light-secondary dark:text-dark-secondary'>
