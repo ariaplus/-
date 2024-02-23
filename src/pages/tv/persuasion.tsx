f8eaaa93b6dd44f43b3daf150521c1fa0dfedf98
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-02-24 01:30:33 +1200

    Update persuasion.tsx

--- src/pages/tv/persuasion.tsx
+++ src/pages/tv/persuasion.tsx
@@ -88,7 +88,10 @@ export default function Bookmarks(): JSX.Element {
       <div className='mb-0'>
   <h2 className='text-base font-bold pl-8 mb-0.5'>Season 1</h2>
   <StrangeCriminalsSeason1/>
-
+        </div>
+     <div className='mb-0'>
+  <h2 className='text-base font-bold pl-8 mb-0.5'>Season 1</h2>
+  <StrangeCriminalsSeason1/>
         </div>
    <Movie/>
     </MainContainer>
