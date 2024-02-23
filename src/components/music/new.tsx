b2cc8bbe580d50079ec7797df5ccf2b14b058f26
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-02-24 02:59:11 +1200

    Update new.tsx

--- src/components/music/new.tsx
+++ src/components/music/new.tsx
@@ -1,25 +1,25 @@
 import Link from 'next/link';
 
-interface EpisodeProps {
+interfaceProps {
   title: string;
   image: string;
-  description: string;
+  description: string
   href: string;
 }
 
-const Music = ({ title, image, description, href }: EpisodeProps) => {
-  const backgroundImage = `url(${image})`;
-
+const Music = ({ title, image, description, href } EpisodeProps) => {
   return (
-      <Link className='w-48 h-48 rounded-lg border-solid border-white bg-opacity-50' href={href}>
-    <div className='bg-cover bg-center p-4 m-2 w-64 h-48 opacity-100 rounded-lg border-solid border-white bg-opacity-50' style={{ backgroundImage }}>
-        <a className='h-32'>
-          <h2 className='text-sm w-52 font-bold mt-4 opacity-100 '>{title}</h2>
-          <p className='text-gray-600 text-xs mt-2 opacity-100'>{description}</p>
-        </a>
-          </div>
-      </Link>
-
+    <Link href={href}>
+      <div className="group relative rounded-lg overflow-hidden">
+        <div className="aspect-w-1 aspect-h-1 group-hover:opacity-75">
+          <img src={image} alt={title} className="object-cover object-center w-full h-full" />
+        </div>
+        <div className="absolute inset-0 flex flex-col p-4">
+          <h2 className="text-white truncate">{title}</h2>
+          <p className="text-gray-300 mt-2">{description}</p>
+        </div>
+      </div>
+    </Link>
   );
 };
 
@@ -30,18 +30,6 @@ export function NewMusicSlide() {
       image: '/assets/tv/movie/data/persuasion/persuasion-cover.jpg',
       description: 'A disaster on board the International Space Station becomes a fight for survival.',
       href: '/tv/strangecriminals/episode/thewoundedangel'
-    },
-     {
-      title: 'The Wounded Angel',
-      image: '/assets/tv/movie/data/persuasion/persuasion-cover.jpg',
-      description: 'A disaster on board the International Space Station becomes a fight for survival.',
-      href: '/songs/season1/episode1'
-    },
-    {
-      title: 'The Wounded Angel',
-      image: '/assets/tv/movie/data/persuasion/persuasion-cover.jpg',
-      description: 'A disaster on board the International Space Station becomes a fight for survival.',
-      href: '/songs/season1/episode1'
     },
     {
       title: 'Live And Let Die',
@@ -64,7 +52,7 @@ export function NewMusicSlide() {
   ];
 
   return (
-    <div className='flex overflow-x-scroll no-scrollbar space-x-4 p-4'>
+    <div className="flex overflow-x-scroll no-scrollbar space-x-4 p-4">
       {songs.map((music) => (
         <Music key={music.href} {...music} />
       ))}
