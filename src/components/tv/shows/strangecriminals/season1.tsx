a6d289245aa69ed1f350fe69f77819bae8b9980b
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-02-24 00:54:42 +1200

    Update season1.tsx

--- src/components/tv/shows/strangecriminals/season1.tsx
+++ src/components/tv/shows/strangecriminals/season1.tsx
@@ -11,8 +11,8 @@ const Episode = ({ title, image, description, href }: EpisodeProps) => {
   const backgroundImage = `url(${image})`;
 
   return (
-      <Link className='w-48 h-32' href={href}>
-    <div className='bg-cover bg-center p-4 m-2 w-64 h-32 opacity-50 rounded-lg border-solid border-white' style={{ backgroundImage }}>
+      <Link className='w-48 h-32 rounded-lg border-solid border-white' href={href}>
+    <div className='bg-cover bg-center p-4 m-2 w-64 h-32 opacity-100 rounded-lg border-solid border-white' style={{ backgroundImage }}>
         <a className='h-32'>
           <h2 className='text-sm w-52 font-bold mt-4 opacity-100 '>{title}</h2>
           <p className='text-gray-600 text-xs mt-2 opacity-100'>{description}</p>
