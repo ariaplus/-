b51515012d462c9d9cb8b45f0ad9c48317edf13a
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-02-24 03:53:42 +1200

    Update new.tsx

--- src/components/music/new.tsx
+++ src/components/music/new.tsx
@@ -12,7 +12,7 @@ const Music = ({ title, image, description, href }: EpisodeProps) => {
 
   return (
       <Link className='w-28 h-28 rounded-lg border-solid border-white bg-opacity-50 overflow-visible' href={href}>
-    <div className='bg-cover bg-center p-4 m-2 w-64 h-64 opacity-100 rounded-lg border-solid border-white bg-opacity-50' style={{ backgroundImage }}>
+    <div className='bg-cover bg-center p-4 m-2 w-64 h-40 opacity-100 rounded-lg border-solid border-white bg-opacity-50' style={{ backgroundImage }}>
         <a className='h-full'>
           <h2 className='text-sm w-28 font-bold mt-28 opacity-100 '>{title}</h2>
         </a>
