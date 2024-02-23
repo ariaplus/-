ef1687954a981d1026cc63454ea1bb1398b85825
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-02-24 02:34:30 +1200

    Update music.tsx

--- src/pages/music.tsx
+++ src/pages/music.tsx
@@ -12,14 +12,24 @@ import { Button } from '@components/ui/button';
 import { ToolTip } from '@components/ui/tooltip';
 import { HeroIcon } from '@components/ui/hero-icon';
 import type { ReactElement, ReactNode } from 'react';
+import { useAuth } from '@lib/context/auth-context';
 
 export default function Bookmarks(): JSX.Element {
   const { back } = useRouter();
+  const { user } = useAuth();
 
+  const userId = user?.id as string;
+  
   return (
     <MainContainer>
       <SEO title='Music | Aria+' />
-      <MainHeader useActionButton title='Music' action={back}>
+      <MainHeader useActionButton action={back}>
+          <div className='-mb-1 flex flex-col'>
+          <h2 className='-mt-1 text-xl font-bold'>Aria+ Music</h2>
+          <p className='text-xs text-light-secondary dark:text-dark-secondary'>
+            @{user?.username}
+          </p>
+        </div>
         <Button
           className='dark-bg-tab group relative ml-auto cursor-not-allowed p-2 hover:bg-light-primary/10
                      active:bg-light-primary/20 dark:hover:bg-dark-primary/10 dark:active:bg-dark-primary/20'
