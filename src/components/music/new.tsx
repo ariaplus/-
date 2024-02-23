ab8b5f0f2f122fcd5de34c07828bf5e7d7511218
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-02-24 04:37:06 +1200

    Update new.tsx

--- src/components/music/new.tsx
+++ src/components/music/new.tsx
@@ -30,32 +30,32 @@ export function NewMusicSlide() {
       href: '/music/listen/goodforyou'
     },
     {
-      title: 'The Wounded Angel',
-      image: '/assets/tv/movie/data/persuasion/persuasion-cover.jpg',
+      title: 'Single Soon (Selena Gomez)',
+      image: 'https://m.media-amazon.com/images/I/51yXKzg19-L._UX250_FMwebp_QL85_.jpg',
       description: 'A disaster on board the International Space Station becomes a fight for survival.',
       href: '/songs/season1/episode1'
     },
     {
-      title: 'The Wounded Angel',
-      image: '/assets/tv/movie/data/persuasion/persuasion-cover.jpg',
+      title: 'Lose You to Love Me',
+      image: 'https://m.media-amazon.com/images/I/51U37LpKVSL._UX250_FMwebp_QL85_.jpg',
       description: 'A disaster on board the International Space Station becomes a fight for survival.',
       href: '/songs/season1/episode1'
     },
     {
-      title: 'Live And Let Die',
-      image: '/assets/tv/movie/data/persuasion/persuasion-cover.jpg',
+      title: 'Hands To Myself (Remixes) Selena Gomez',
+      image: 'https://m.media-amazon.com/images/I/51R+KpJ1S1L._UX250_FMwebp_QL85_.jpg',
       description: 'Jo races to find a way back to Earth before her life support runs out. Henry is concerned about his experiment.',
       href: '/songs/season1/episode2'
     },
     {
-      title: 'Somewhere in Space Hangs My Heart',
-      image: '/assets/tv/movie/data/persuasion/persuasion-cover.jpg',
+      title: 'Good For You Selena Gomez [feat. A$AP Rocky]',
+      image: 'https://m.media-amazon.com/images/I/61M0jgA4w7L._UX250_FMwebp_QL85_.jpg',
       description: 'The space agencies begin their investigation into the ISS collision. Jo struggles to reconnect.',
       href: '/songs/season1/episode3'
     },
     {
-      title: 'The Left Hand of God',
-      image: '/assets/tv/movie/data/persuasion/persuasion-cover.jpg',
+      title: 'For You (Selena Gomez)',
+      image: 'https://m.media-amazon.com/images/I/41gTW2MhVGL._UX250_FMwebp_QL85_.jpg',
       description: 'Jo\'s life back home is not how she remembers it, and growing tension with Magnus adds to her feelings of alienation.',
       href: '/songs/season1/episode4'
     }
