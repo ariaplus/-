e8b33910cee142234cfbd1591b0cd01330bead0f
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-02-26 20:49:47 +1200

    Update explore.tsx

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -38,7 +38,7 @@ export default function Home(): JSX.Element {
       >
     <div className=' flex items-left'>
             <ExploreButton />
-               <p className='mt-1 text-base'>Explore</p>
+               <p className='mt-2 text-base'>Explore</p>
     </div>
       <div className=' flex items-right'>
         <DiscoverButton />
