091e8831a9ddb80f4fd1f3319d1d3026b4866019
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-01 23:53:57 +1200

    Update sidebar.tsx

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -117,7 +117,7 @@ export function Sidebar(): JSX.Element {
               <SidebarLink {...linkData} key={linkData.href} />
             ))}
             <SidebarLink
-              href={`/user/${username}`}
+              href={`/${username}`}
               username={username}
               linkName='Profile'
               iconName='UserIcon'
@@ -125,7 +125,7 @@ export function Sidebar(): JSX.Element {
             {!isMobile && <MoreSettings />}
           </nav>
           <Button
-            className='accent-tab place-self-center self-center justify-self-center justify-center object-center items-center absolute right-4 -translate-y-[72px] bg-main-accent text-lg font-bold text-white
+            className='group accent-tab place-self-center self-center justify-self-center justify-center object-center items-center absolute right-4 -translate-y-[72px] bg-main-accent text-lg font-bold text-white
                        outline-none transition hover:brightness-90 active:brightness-75 xs:static xs:translate-y-0
                        xs:hover:bg-main-accent/90 xs:active:bg-main-accent/75 xl:w-11/12'
             onClick={openModal}
