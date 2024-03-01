8880bfd1689fcbccf02d9148fc37daf3e29308d4
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-01 23:01:07 +1200

    Update start.tsx

--- src/components/modal/discover/start.tsx
+++ src/components/modal/discover/start.tsx
@@ -1,82 +1,49 @@
-import { Dialog } from '@headlessui/react';
-import { CustomIcon } from '@components/ui/custom-icon';
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { useState, useEffect } from 'react';
+import { toast } from 'react-hot-toast';
+import { checkUsernameAvailability, updateUsername } from '@lib/firebase/utils';
+import { useAuth } from '@lib/context/auth-context';
+import { useModal } from '@lib/hooks/useModal';
+import { isValidUsername } from '@lib/validation';
+import { sleep } from '@lib/utils';
 import { Button } from '@components/ui/button';
-import type { ReactNode, FormEvent } from 'react';
-import { ArplImage } from '@components/ui/ariaplus';
-import Image from 'next/image';
-import Link from 'next/link';
-import { NextImage } from '@components/ui/next-image';
+import { HeroIcon } from '@components/ui/hero-icon';
+import { ToolTip } from '@components/ui/tooltip';
+import { Modal } from '@components/modal/modal';
+import { StartModal } from '@components/modal/discover/start-modal';
+import type { FormEvent, ChangeEvent } from 'react';
 
-interface ImageProps {
-  src: string;
-  href: string;
-}
+export function StartModal(): JSX.Element {
 
 
+  const { user } = useAuth();
+  const { open, openModal, closeModal } = useModal();
 
-export function StartModal( ): JSX.Element {
+  const cancelStart = (): void => {
+    closeModal();
+    if (!alreadySet) void updateUsername(user?.id as string);
+  };
 
-  
-  const images: ImageProps[] = [
-   { src: '/assets/tv/movie/m.jpg', href: '/tv/forestwitch' },
-   { src: '/assets/tv/movie/n.jpg', href: '/tv/nowhere' },
-   { src: '/assets/tv/movie/n.jpg', href: '/tv/nowhere' },
-   { src: '/assets/tv/movie/twtc.jpg', href: '/link1' },
-   { src: '/assets/tv/movie/m.jpg', href: '/tv/forestwitch' },
-   { src: '/assets/tv/movie/n.jpg', href: '/tv/nowhere' },
-   { src: '/assets/tv/movie/n.jpg', href: '/tv/nowhere' },
-   { src: '/assets/tv/movie/twtc.jpg', href: '/link1' },
-   { src: '/assets/tv/movie/m.jpg', href: '/tv/forestwitch' },
-   { src: '/assets/tv/movie/n.jpg', href: '/tv/nowhere' },
-   { src: '/assets/tv/movie/twtc.jpg', href: '/link1' },
-   { src: '/assets/tv/movie/gits.jpeg', href: '/link1' }   
-    // Add more images as needed
-  ];
+
   return (
-    <form
-      className='flex h-full flex-col justify-between bg-no-repeat bg-top bg-cover'>
-    <div className='flex flex-col items-center gap-6'>
-        <div className='flex flex-col gap-4'>
-          <i className='mx-auto'>
-      <ArplImage
-          imgClassName='arplicon'
-          blurClassName='none'
-          src='/assets/ariaplus.svg'
-          alt='Twitter banner'
-          layout='fill'
-          width='40px'
-          height='40px'
-          useSkeleton
-       />
-          </i>
+    <>
+      <Modal
+        modalClassName='flex flex-col gap-6 max-w-xl bg-main-background w-full p-8 rounded-2xl h-[485px] bg-no-repeat bg-top bg-cover' 
+        open={open}
+        closeModal={cancelStart}
+      >
 
-        </div>
-      </div>
-     <div className='grid grid-cols-4 gap-4 px-3'>
-      {images.map((image, index) => (
-        <Link key={index} href={image.href}>
-          <a className='relative h-16 md:h-16 sm:h-16 overflow-hidden hover-animation transition rounded-lg'>
-            <NextImage
-              useSkeleton
-              src={image.src}
-              layout='fill'
-              objectFit='cover'
-              alt={`Grid Image ${index + 1}`}
-            />
-          </a>
-        </Link>
-      ))}
-    </div>
-      <div className='flex flex-col gap-3 inner:py-2 inner:font-bold'>
-
-        <Button
-          className='border border-light-line-reply hover:bg-light-primary/10 focus-visible:bg-light-primary/10
-                     active:bg-light-primary/20 dark:border-light-secondary dark:text-light-border 
-                     dark:hover:bg-light-border/10 dark:focus-visible:bg-light-border/10 
-                     dark:active:bg-light-border/20'
-          onClick={cancelStart}
-        >Close</Button>
-      </div>
-    </form>
+      </Modal>
+      <Button
+        className='dark-bg-tab group relative p-2 hover:bg-light-primary/10
+                   active:bg-light-primary/20 dark:hover:bg-dark-primary/10 
+                   dark:active:bg-dark-primary/20'
+        onClick={openModal}
+      >
+        <HeroIcon className='h-5 w-5' iconName='RectangleStackIcon' />
+        <ToolTip tip='Top tweets' />
+      </Button>
+    </>
   );
 }
