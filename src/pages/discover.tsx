a4382de917dfda6cb135228a6a73f8beb40a6802
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-01 19:32:33 +1200

    Create discover.tsx

---
+++ src/pages/discover.tsx
@@ -0,0 +1,77 @@
+import { AnimatePresence } from 'framer-motion';
+import { where, orderBy } from 'firebase/firestore';
+import { useWindow } from '@lib/context/window-context';
+import { useInfiniteScroll } from '@lib/hooks/useInfiniteScroll';
+import { tweetsCollection } from '@lib/firebase/collections';
+import { HomeLayout, ProtectedLayout } from '@components/layout/common-layout';
+import { MainLayout } from '@components/layout/main-layout';
+import { SEO } from '@components/common/seo';
+import { MainContainer } from '@components/home/main-container';
+import { Input } from '@components/input/input';
+import { UpdateUsername } from '@components/home/update-username';
+import { MainHeader } from '@components/home/main-header';
+import { Tweet } from '@components/tweet/tweet';
+import { Loading } from '@components/ui/loading';
+import { Error } from '@components/ui/error';
+import { DiscoverButton } from '@components/explore/header/discover-button';
+import { ExploreButton } from '@components/explore/header/explore-button';
+import type { ReactElement, ReactNode } from 'react';
+import { NextImage } from '@components/ui/next-image';
+
+export default function Home(): JSX.Element {
+  const { isMobile } = useWindow();
+
+  const { data, loading, LoadMore } = useInfiniteScroll(
+    tweetsCollection,
+    [where('parent', '==', null), orderBy('createdAt', 'desc')],
+    { includeUser: true, allowNull: true, preserve: true }
+  );
+
+  return (
+    <MainContainer>
+      <SEO title='Discover | Aria+' />
+      <MainHeader
+        useMobileSidebar
+        title='Discover'
+        className='flex items-center justify-between text-[0px]'
+      > <div className='flex items-right' >
+        <ExploreButton />
+        <DiscoverButton />
+         </div>
+      </MainHeader>
+      {!isMobile && <Input />}
+     <NextImage className='accent-tab relative mt-0.5 h-36 xs:h-48 sm:h-52 h-full w-full rounded-none p-0 transition hover:brightness-75'
+            useSkeleton
+            layout='fill'
+            imgClassName='object-cover'
+            src='https://images.pexels.com/photos/4577718/pexels-photo-4577718.jpeg'
+            alt=''
+            key=''
+          />
+      <section className='mt-0.5 xs:mt-0'>
+        {loading ? (
+          <Loading className='mt-5' />
+        ) : !data ? (
+          <Error message='Something went wrong' />
+        ) : (
+          <>
+            <AnimatePresence mode='popLayout'>
+              {data.map((tweet) => (
+                <Tweet {...tweet} key={tweet.id} />
+              ))}
+            </AnimatePresence>
+            <LoadMore />
+          </>
+        )}
+      </section>
+    </MainContainer>
+  );
+}
+
+Home.getLayout = (page: ReactElement): ReactNode => (
+  <ProtectedLayout>
+    <MainLayout>
+      <HomeLayout>{page}</HomeLayout>
+    </MainLayout>
+  </ProtectedLayout>
+);
