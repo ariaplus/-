e312d71210f0860788a52bac40e5ad79131f1bc4
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-01 22:17:24 +1200

    Update cat-modal.tsx

--- src/components/modal/tv/cat-modal.tsx
+++ src/components/modal/tv/cat-modal.tsx
@@ -5,7 +5,10 @@ import type { ReactNode, FormEvent } from 'react';
 import { ArplImage } from '@components/ui/ariaplus';
 import { Cat } from '@components/tv/cat';
 
-
+interface ImageProps {
+  src: string;
+  href: string;
+}
 
 type UsernameModalProps = {
   loading: boolean;
@@ -39,7 +42,22 @@ export function UsernameModal({
   cancelUpdateUsername
 }: UsernameModalProps): JSX.Element {
   const { title, description, cancelLabel } = usernameModalData[+alreadySet];
-
+  
+  const images: ImageProps[] = [
+   { src: '/assets/tv/movie/m.jpg', href: '/tv/forestwitch' },
+   { src: '/assets/tv/movie/n.jpg', href: '/tv/nowhere' },
+   { src: '/assets/tv/movie/n.jpg', href: '/tv/nowhere' },
+   { src: '/assets/tv/movie/twtc.jpg', href: '/link1' },
+   { src: '/assets/tv/movie/m.jpg', href: '/tv/forestwitch' },
+   { src: '/assets/tv/movie/n.jpg', href: '/tv/nowhere' },
+   { src: '/assets/tv/movie/n.jpg', href: '/tv/nowhere' },
+   { src: '/assets/tv/movie/twtc.jpg', href: '/link1' },
+   { src: '/assets/tv/movie/m.jpg', href: '/tv/forestwitch' },
+   { src: '/assets/tv/movie/n.jpg', href: '/tv/nowhere' },
+   { src: '/assets/tv/movie/twtc.jpg', href: '/link1' },
+   { src: '/assets/tv/movie/gits.jpeg', href: '/link1' }   
+    // Add more images as needed
+  ];
   return (
     <form
       className='flex h-full flex-col justify-between bg-no-repeat bg-top bg-cover'>
@@ -66,7 +84,21 @@ export function UsernameModal({
         </div>
         {children}
       </div>
-      <Cat/>
+     <div className='grid grid-cols-4 gap-4 px-3'>
+      {images.map((image, index) => (
+        <Link key={index} href={image.href}>
+          <a className='relative h-16 md:h-16 sm:h-16 overflow-hidden hover-animation transition rounded-lg'>
+            <NextImage
+              useSkeleton
+              src={image.src}
+              layout='fill'
+              objectFit='cover'
+              alt={`Grid Image ${index + 1}`}
+            />
+          </a>
+        </Link>
+      ))}
+    </div>
       <div className='flex flex-col gap-3 inner:py-2 inner:font-bold'>
 
         <Button
