cdf71bbc07b07943823e1def05a984aaca2b4cdf
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-07 00:00:12 +1200

    Update server-down.tsx

--- src/components/maintenance/server-down.tsx
+++ src/components/maintenance/server-down.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 interface CardProps {
   title: string;
   description: string;
-  image: string;
 }
 
 const Card: React.FC<CardProps> = ({ title, description, image }) => {
