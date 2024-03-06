6af2551ddf964b72b34cc9f047b261d6fd8c9c1a
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-07 00:14:37 +1200

    Update server-down.tsx

--- src/components/maintenance/server-down.tsx
+++ src/components/maintenance/server-down.tsx
@@ -7,10 +7,10 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ title, description }) => {
   return (
-    <div className='bg-black rounded-lg shadow-md p-2.5 w-full mx-auto border'>
+    <div className='bg-black rounded-lg shadow-md p-2.5 w-full mx-auto border border-slate-700'>
       <div className='p-4'>
-        <h2 className='text-xl text-center font-semibold mb-2'>{title}</h2>
-        <p className='text-gray-700 text-center mb-4'>{description}</p>
+        <h2 className='text-xl text-slate-500 text-center font-semibold mb-2'>{title}</h2>
+        <p className='text-gray-700 text-[12.5px] text-center mb-4'>{description}</p>
       </div>
     </div>
   );
@@ -21,7 +21,7 @@ export function ServerDown() {
     <div className='container mx-auto p-4'>
       <Card
         title='Aria+ Will Be Back Soon'
-        description="Aria+ is down for required maintenance right now but you should be able to get back on within a few minuites In the meantime read more about why you're seeing this message Thanks for your patience as we improve the platform "
+        description="Aria+ is down for required maintenance right now but you should be able to get back on within a few minuites In the meantime read more about why you're seeing this message. Thanks for your patience as we improve the platform "
       />
     </div>
   );
