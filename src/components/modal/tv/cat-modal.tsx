e5e9282cceae831410d0cb3c4a958dabdf6aeb55
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-07 20:00:13 +1200

    Update cat-modal.tsx

--- src/components/modal/tv/cat-modal.tsx
+++ src/components/modal/tv/cat-modal.tsx
@@ -6,6 +6,7 @@ import { ArplImage } from '@components/ui/ariaplus';
 import Image from 'next/image';
 import Link from 'next/link';
 import { NextImage } from '@components/ui/next-image';
+import { MainHeader } from '@components/home/main-header';
 
 interface ImageProps {
   src: string;
@@ -61,31 +62,14 @@ export function UsernameModal({
     // Add more images as needed
   ];
   return (
-    <form
+      <form
       className='flex h-full flex-col justify-between bg-no-repeat bg-top bg-cover'>
-    <div className='flex flex-col items-center gap-6'>
-        <div className='flex flex-col gap-4'>
-          <i className='mx-auto'>
-      <ArplImage
-          imgClassName='arplicon'
-          blurClassName='none'
-          src='/assets/ariaplus.svg'
-          alt='Twitter banner'
-          layout='fill'
-          width='40px'
-          height='40px'
-          useSkeleton
-       />
-          </i>
-          <div className='flex items-center flex-col gap-2'>
-            <Dialog.Title className='text-2xl font-bold xs:text-3xl sm:text-4xl'>
-              {title}
-            </Dialog.Title>
-
-          </div>
-        </div>
-        {children}
-      </div>
+      <MainHeader
+        title='Start'
+        className='flex items-center justify-between text-[0px]'
+      > <div className='flex items-right' >
+         </div>
+      </MainHeader>
      <div className='grid grid-cols-4 gap-4 px-3'>
       {images.map((image, index) => (
         <Link key={index} href={image.href}>
@@ -100,17 +84,17 @@ export function UsernameModal({
           </a>
         </Link>
       ))}
-    </div>
-      <div className='flex flex-col gap-3 inner:py-2 inner:font-bold'>
-
-        <Button
-          className='border border-light-line-reply hover:bg-light-primary/10 focus-visible:bg-light-primary/10
-                     active:bg-light-primary/20 dark:border-light-secondary dark:text-light-border 
-                     dark:hover:bg-light-border/10 dark:focus-visible:bg-light-border/10 
-                     dark:active:bg-light-border/20'
-          onClick={cancelUpdateUsername}
-        >Close</Button>
-      </div>
+     </div>
+        <div className='flex items-center justify-between text-[0px]'>
+                <NextImage className='accent-tab relative mt-0.5 h-16 xs:h-16 sm:h-16 h-16 w-full rounded-none p-0 transition hover:brightness-75'
+            useSkeleton
+            layout='fill'
+            imgClassName='object-cover'
+            src='https://images.pexels.com/photos/4577718/pexels-photo-4577718.jpeg'
+            alt=''
+            key=''
+          />
+        
+        </div>
     </form>
   );
-}
