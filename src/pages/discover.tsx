e9c3b4dd8c77a2e53587e1d61aa5c0af3d94e39d
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-08 22:33:28 +1200

    Update discover.tsx

--- src/pages/discover.tsx
+++ src/pages/discover.tsx
@@ -40,7 +40,7 @@ export default function Home(): JSX.Element {
         <Start />
          </div>
       </MainHeader>
-       <MainHeader>
+       <MainHeader className='flex items-center justify-between z-[9] ' >
               <div className='flex items-center' >
         <NewsButton />
          </div>
