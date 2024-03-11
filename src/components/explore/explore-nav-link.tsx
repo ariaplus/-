208d3ae69217063d8b114d3f781fe7c21c7871f1
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-11 12:14:02 +1200

    Update explore-nav-link.tsx

--- src/components/explore/explore-nav-link.tsx
+++ src/components/explore/explore-nav-link.tsx
@@ -1,19 +1,20 @@
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import cn from 'clsx';
+import { ArplImage } from '@components/ui/ariaplus';
 
 type UserNavLinkProps = {
-  name: string;
+  src: string;
   path: string;
 };
 
-export function UserNavLink({ name, path }: UserNavLinkProps): JSX.Element {
+export function ExploreNavLink({ src, path }: UserNavLinkProps): JSX.Element {
   const {
     asPath,
     query: { id }
   } = useRouter();
 
-  const userPath = `/explore/${path ? `/${path}` : ''}`;
+  const userPath = `/${id as string}${path ? `/${path}` : ''}`;
 
   return (
     <Link href={userPath} scroll={false}>
@@ -30,7 +31,16 @@ export function UserNavLink({ name, path }: UserNavLinkProps): JSX.Element {
                 : 'text-light-secondary dark:text-dark-secondary'
             )}
           >
-            {name}
+                      <ArplImage
+          imgClassName='arplicon'
+          blurClassName='none'
+          src={src}
+          alt=''
+          layout='fill'
+          width='25px'
+          height='25px'
+          useSkeleton
+       />
             <i className='h-1 scale-50 rounded-full bg-main-accent opacity-0 transition duration-200' />
           </p>
         </div>
