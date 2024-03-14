681d96d0f0bbf9061a3aa272b82cb0d65582ea7f
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-15 10:33:43 +1200

    Create fwt.tsx

---
+++ src/components/tv/trailers/fwt.tsx
@@ -0,0 +1,6 @@
+import Video from 'next-video';
+import myVideo from '/assets/tv/trailers/Gísli Gunnarsson - Birds of Paradise.mp4';
+
+export default function fwt() {
+  return <Video src='public/assets/tv/trailers/Gísli Gunnarsson - Birds of Paradise.mp4' />;
+}
