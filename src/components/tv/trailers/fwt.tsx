1373400e9da0a08d4936e18dbdbc58c47ef5d37b
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-15 10:36:14 +1200

    Delete src/components/tv/trailers directory

--- src/components/tv/trailers/fwt.tsx
+++
@@ -1,6 +0,0 @@
-import Video from 'next-video';
-import myVideo from '/assets/tv/trailers/Gísli Gunnarsson - Birds of Paradise.mp4';
-
-export default function fwt() {
-  return <Video src='public/assets/tv/trailers/Gísli Gunnarsson - Birds of Paradise.mp4' />;
-}
