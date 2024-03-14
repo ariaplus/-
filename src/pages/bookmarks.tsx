3b276ca08c8a8ee511a4d18c796a3c5e523171cb
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-15 11:54:06 +1200

    Update bookmarks.tsx

--- src/pages/bookmarks.tsx
+++ src/pages/bookmarks.tsx
@@ -73,8 +73,8 @@ export default function Bookmarks(): JSX.Element {
           closeModal={closeModal}
         />
       </Modal>
-      <MainHeader className='flex items-center justify-between'>
-        <div className='-mb-1 flex flex-col'>
+          <MainHeader useActionButton action={back} className='flex items-center '>
+        <div className='-mb-1 justify-between flex flex-col'>
           <h2 className='-mt-1 text-xl font-bold'>Bookmarks</h2>
           <p className='text-xs text-light-secondary dark:text-dark-secondary'>
             @{user?.username}
