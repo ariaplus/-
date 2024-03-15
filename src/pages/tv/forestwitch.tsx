acfc3871ea29b1e7b7a07b5f8dd592f3a44a59e4
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-16 01:07:43 +1200

    Update forestwitch.tsx

--- src/pages/tv/forestwitch.tsx
+++ src/pages/tv/forestwitch.tsx
@@ -25,13 +25,7 @@ export default function Bookmarks(): JSX.Element {
     <MainContainer>
       <SEO title='Forest Witch | Aria+' />
       <MainHeader useActionButton title='Forest Witch' action={back}>
-        <Button
-          className='dark-bg-tab group relative ml-auto cursor-not-allowed p-2 hover:bg-light-primary/10
-                     active:bg-light-primary/20 dark:hover:bg-dark-primary/10 dark:active:bg-dark-primary/20'
-        >
-          <HeroIcon className='h-5 w-5' iconName='TicketIcon' />
-          <ToolTip tip='Settings' />
-        </Button>
+
       </MainHeader>
       
     <div className='flex flex-col items-center p-8 bg-no-repeat bg-top bg-contain -mt-[100px]' style={{ backgroundImage: 'url(/assets/tv/movie/data/forestwitch/cover.jpg)' }}>
@@ -43,16 +37,16 @@ export default function Bookmarks(): JSX.Element {
         height={125}  
         className=' mt-56 rounded-lg pt-20'
                />
-      <p className='text-gray-600 mt-4 text-center'>Horror 2024 | 1hr 40min | 4K</p>
-      <p className='text-yellow-600 text-center'>Comming to Aria+ at a Later Date</p>
-      <div className='flex'>
+      <p className='text-gray-600 mt-4 text-center'>Horror | 2024 | 1hr 40min | 4K</p>
+      <p className='text-yellow-600 text-center'>Coming to Aria+ at a Later Date</p>
+      <div className='flex pt-[7px] gap-[10px] ml-[40px] '>
       <Button
     className='accent-tab bg-main-accent enabled:hover:bg-main-accent/90
-                     enabled:active:bg-main-accent/75 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 w-80 rounded-lg'>Play</Button>
+                     enabled:active:bg-main-accent/75 hover:bg-blue-700 text-white font-bold py-2 px-4 w-80 rounded-lg'>Play</Button>
       <TvPassModal/  >   
         </div>         
         
-      <p className='text-gray-600 mt-4 text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce luctus mollis ex a sodales. Quisque molestie massa quis dolor elementum rhoncus. Nullam scelerisque elementum elit, nec vestibulum augue luctus id. Aliquam et dolor non neque pulvinar sodales a nec mi. Morbi varius a erat in fermentum. Donec sed scelerisque purus. Integer sed libero sit amet erat aliquet facilisis in quis augue. Sed sollicitudin egestas fermentum. Integer sit amet tincidunt velit. Maecenas tincidunt justo at erat luctus feugiat. Pellentesque eget diam vitae elit ultrices ullamcorper. Aliquam tincidunt quam elit, ut interdum nisi maximus in. Donec eget enim feugiat, rutrum turpis sit amet, egestas tortor. Aenean lobortis finibus hendrerit. </p>
+      <p className='text-gray-600 pt-[10px] text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce luctus mollis ex a sodales. Quisque molestie massa quis dolor elementum rhoncus. Nullam scelerisque elementum elit, nec vestibulum augue luctus id. Aliquam et dolor non neque pulvinar sodales a nec mi. Morbi varius a erat in fermentum. Donec sed scelerisque purus. Integer sed libero sit amet erat aliquet facilisis in quis augue. Sed sollicitudin egestas fermentum. Integer sit amet tincidunt velit. Maecenas tincidunt justo at erat luctus feugiat. Pellentesque eget diam vitae elit ultrices ullamcorper. Aliquam tincidunt quam elit, ut interdum nisi maximus in. Donec eget enim feugiat, rutrum turpis sit amet, egestas tortor. Aenean lobortis finibus hendrerit. </p>
      </div>
      <div className='border-solid border-2 border-white mx-3.5 mb-5 shadow-md rounded-md p-8'>
       <h2 className='text-2xl font-bold mb-2'>Forest Witch</h2>
