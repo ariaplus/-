54ead42829976070f08636eda35604b834c4d78c
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-21 18:29:13 +1200

    Update explore-nav-link.tsx

--- src/components/explore/explore-nav-link.tsx
+++ src/components/explore/explore-nav-link.tsx
@@ -26,17 +26,12 @@ export function ExploreNavLink({ name, src, path }: ExploreNavLinkProps): JSX.El
         className='hover-animation main-tab dark-bg-tab flex flex-1 justify-center
                    hover:bg-light-primary/10 dark:hover:bg-dark-primary/10'
       >
+        
+        <ToolTip tip={name} />
         <div className='px-[8px] md:px-[16px]'>
           
         
-          <p
-            className={cn(
-              'flex flex-col gap-3 whitespace-nowrap pt-3 font-bold transition-colors duration-200',
-              asPath === userPath
-                ? 'text-light-primary dark:text-dark-primary [&>i]:scale-100 [&>i]:opacity-100'
-                : 'text-light-secondary dark:text-dark-secondary'
-            )}
-          >
+     
 
          <ToolTip tip={name} />
             <ArplImage
@@ -50,7 +45,6 @@ export function ExploreNavLink({ name, src, path }: ExploreNavLinkProps): JSX.El
               useSkeleton
             />
             <i className='h-1 scale-50 rounded-full bg-main-accent opacity-0 transition duration-200' />
-          </p>
         </div>
       </a>
     </Link>
