1aec1833ad300197278b08043f60e8d2f04e2177
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-21 18:43:02 +1200

    Update explore-nav-link.tsx

--- src/components/explore/explore-nav-link.tsx
+++ src/components/explore/explore-nav-link.tsx
@@ -11,34 +11,27 @@ type ExploreNavLinkProps = {
 };
 
 export function ExploreNavLink({ name, src, path }: ExploreNavLinkProps): JSX.Element {
+  const router = useRouter();
   const {
     asPath,
     query: { id }
-  } = useRouter();
+  } = router;
 
   const userPath = path ? `/${path}` : '';
 
-  return (
-    <Link href={userPath} scroll={false}>
+  const handleClick = () => {
+    router.push(userPath);
+  };
 
-      
-    
-      
-      <a
+  return (
+    <div>
+      <button
         className='hover-animation main-tab dark-bg-tab flex flex-1 justify-center
                    hover:bg-light-primary/10 dark:hover:bg-dark-primary/10'
+        onClick={handleClick}
       >
-        
-
-        
         <div className='px-[8px] md:px-[16px]'>
-
-            <ToolTip
-                    className='translate-y-1 z-50' // Apply z-index to bring tooltip to the top
-                    tip={name}
-                  />
-         
-                  <p
+          <p
             className={cn(
               'flex flex-col gap-3 whitespace-nowrap pt-3 font-bold transition-colors duration-200',
               asPath === userPath
@@ -46,10 +39,6 @@ export function ExploreNavLink({ name, src, path }: ExploreNavLinkProps): JSX.El
                 : 'text-light-secondary dark:text-dark-secondary'
             )}
           >
-     
-
-
-                  
             <ArplImage
               imgClassName='arplicon'
               blurClassName='none'
@@ -63,7 +52,12 @@ export function ExploreNavLink({ name, src, path }: ExploreNavLinkProps): JSX.El
             <i className='h-1 scale-50 rounded-full bg-main-accent opacity-0 transition duration-200' />
           </p>
         </div>
-      </a>
-    </Link>
+      </button>
+      <Link href={userPath} scroll={false}>
+        <a className='sr-only'>
+          <ToolTip tip={name} />
+        </a>
+      </Link>
+    </div>
   );
 }
