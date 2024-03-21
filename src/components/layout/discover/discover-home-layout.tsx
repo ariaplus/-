9becbc75c7ffbf00c803fb8e8fbed3da3518c720
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-21 13:19:58 +1200

    Create discover-home-layout.tsx

---
+++ src/components/layout/discover/discover-home-layout.tsx
@@ -0,0 +1,57 @@
+import { useRouter } from 'next/router';
+import { motion } from 'framer-motion';
+import { useAuth } from '@lib/context/auth-context';
+import { useUser } from '@lib/context/user-context';
+import { SEO } from '@components/common/seo';
+import { UserHomeCover } from '@components/user/user-home-cover';
+import { UserHomeAvatar } from '@components/user/user-home-avatar';
+import { UserDetails } from '@components/user/user-details';
+import { UserNav } from '@components/user/user-nav';
+import { Button } from '@components/ui/button';
+import { Loading } from '@components/ui/loading';
+import { HeroIcon } from '@components/ui/hero-icon';
+import { ToolTip } from '@components/ui/tooltip';
+import { FollowButton } from '@components/ui/follow-button';
+import { variants } from '@components/user/user-header';
+import { UserEditProfile } from '@components/user/user-edit-profile';
+import { UserShare } from '@components/user/user-share';
+import type { LayoutProps } from './common-layout';
+import { UpdateUsername } from '@components/home/update-username';
+import { MessageButton } from '@components/user/buttons/message-button';
+
+export function DiscoverHomeLayout({ children }: LayoutProps): JSX.Element {
+  const { user, isAdmin } = useAuth();
+  const { user: userData, loading } = useUser();
+
+  const {
+    query: { id }
+  } = useRouter();
+
+  const coverData = userData?.coverPhotoURL
+    ? { src: userData.coverPhotoURL, alt: userData.name }
+    : null;
+
+  const profileData = userData
+    ? { src: userData.photoURL, alt: userData.name }
+    : null;
+
+  const { id: userId } = user ?? {};
+
+  const isOwner = userData?.id === userId;
+
+  return (
+    <>
+        <SEO
+          title=' Discover | Aria+`}
+        />
+     
+     
+      {userData && (
+        <>
+          <UserNav />
+          {children}
+        </>
+      )}
+    </>
+  );
+}
