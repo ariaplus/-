44da19be0d4a9dee2ff0aa1d3daf60902c99bb54
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-21 12:52:10 +1200

    Update explore-nav-link.tsx

--- src/components/explore/explore-nav-link.tsx
+++ src/components/explore/explore-nav-link.tsx
@@ -1,23 +1,21 @@
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import cn from 'clsx';
-import { ToolTip } from '@components/ui/tooltip';
-
 import { ArplImage } from '@components/ui/ariaplus';
 
-type UserNavLinkProps = {
+type ExploreNavLinkProps = {
+  name: string;
   src: string;
   path: string;
-  name: string;
 };
 
-export function ExploreNavLink({ name, src, path }: UserNavLinkProps): JSX.Element {
+export function ExploreNavLink({ name, src, path }: ExploreNavLinkProps): JSX.Element {
   const {
     asPath,
     query: { id }
   } = useRouter();
 
-  const userPath = `/${path ? `/${path}` : ''}`;
+  const userPath = `/${id as string}${path ? `/${path}` : ''}`;
 
   return (
     <Link href={userPath} scroll={false}>
@@ -25,7 +23,7 @@ export function ExploreNavLink({ name, src, path }: UserNavLinkProps): JSX.Eleme
         className='hover-animation main-tab dark-bg-tab flex flex-1 justify-center
                    hover:bg-light-primary/10 dark:hover:bg-dark-primary/10'
       >
-        <div className='pl-[10px] pr-[10px]'>
+        <div className='px-6 md:px-8'>
           <p
             className={cn(
               'flex flex-col gap-3 whitespace-nowrap pt-3 font-bold transition-colors duration-200',
@@ -34,6 +32,10 @@ export function ExploreNavLink({ name, src, path }: UserNavLinkProps): JSX.Eleme
                 : 'text-light-secondary dark:text-dark-secondary'
             )}
           >
+
+            {name}
+
+            
                       <ArplImage
           imgClassName='arplicon'
           blurClassName='none'
@@ -44,7 +46,6 @@ export function ExploreNavLink({ name, src, path }: UserNavLinkProps): JSX.Eleme
           height='25px'
           useSkeleton
        />
-          <ToolTip tip={name} />
             <i className='h-1 scale-50 rounded-full bg-main-accent opacity-0 transition duration-200' />
           </p>
         </div>
