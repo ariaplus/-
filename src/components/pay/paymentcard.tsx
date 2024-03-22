07642a432bf897cb51ed8744112e8bc3b7ca16f8
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-23 01:14:50 +1200

    Update paymentcard.tsx

--- src/components/pay/paymentcard.tsx
+++ src/components/pay/paymentcard.tsx
@@ -9,7 +9,7 @@ const userId = user?.id as string;
   
   return (
     <Link href='/pay/flow'>
-      <a className='block bg-cover border-solid border-2 border-white bg-center h-[13rem] w-[350px] xs:h-[13rem] sm:h-64 sm:w-[350px] rounded-2xl shadow-md' style={{ backgroundImage: 'url(/assets/blue.jpg)' }}>
+      <a className='block bg-cover border-solid border-2 border-white bg-center h-[13rem] w-[350px] xs:h-[13rem] sm:h-[13rem] sm:w-[350px] rounded-2xl shadow-md' style={{ backgroundImage: 'url(/assets/blue.jpg)' }}>
 
           <div className='flex p-4 items:end content-end '>
         <p className='pl-2 text-xl text-light-secondary dark:text-dark-secondary'>
