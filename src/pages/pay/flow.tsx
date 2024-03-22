a51fcaa47ed03ca0308de446b8066050711954f6
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-22 23:54:44 +1200

    Create flow.tsx

---
+++ src/pages/pay/flow.tsx
@@ -0,0 +1,56 @@
+import { useRouter } from 'next/router';
+import { useAuth } from '@lib/context/auth-context';
+import {
+  TrendsLayout,
+  ProtectedLayout
+} from '@components/layout/common-layout';
+import {
+  moviesCollection,
+  userBookmarksCollection
+} from '@lib/firebase/collections';
+import { MainLayout } from '@components/layout/main-layout';
+import { SEO } from '@components/common/seo';
+import { MainHeader } from '@components/home/main-header';
+import { MainContainer } from '@components/home/main-container';
+import { AsideTrends } from '@components/aside/aside-trends';
+import { Button } from '@components/ui/button';
+import { ToolTip } from '@components/ui/tooltip';
+import { HeroIcon } from '@components/ui/hero-icon';
+import { CardsButton } from '@components/pay/cards-button';
+import { PaymentsButton } from '@components/pay/payments-button';
+import { WalletButton } from '@components/pay/wallet-button';
+import { PaymentCard } from '@components/pay/paymentcard';
+import { PaySettingsButton } from '@components/pay/pay-settings-button';
+import { PayNav } from '@components/navbar/primary/pay/pay-nav';
+import type { ReactElement, ReactNode } from 'react';
+
+export default function Pay(): JSX.Element {
+  const { back } = useRouter();
+  
+ const { user } = useAuth();
+  const userId = user?.id as string;
+  
+  return (
+    <MainContainer>
+      <SEO title='Make Payment | Aria+' />
+      <MainHeader     useActionButton action={back} 
+        title='Pay'
+        className='flex items-center justify-between text-[0px]'
+      > 
+       <PaySettingsButton/>
+      </MainHeader>
+      <PayNav/>
+      <div className='px-8'>
+      <PaymentCard />
+        </div>
+    </MainContainer>
+  );
+}
+
+Pay.getLayout = (page: ReactElement): ReactNode => (
+  <ProtectedLayout>
+    <MainLayout>
+      <TrendsLayout>{page}</TrendsLayout>
+    </MainLayout>
+  </ProtectedLayout>
+);
