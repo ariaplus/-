1c99fd796b1991f160f557edf7c811bc230251ac
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-22 23:59:33 +1200

    Create flow-nav.tsx

---
+++ src/components/navbar/primary/pay/flow-nav.tsx
@@ -0,0 +1,40 @@
+import { motion } from 'framer-motion';
+import cn from 'clsx';
+import { variants } from '@components/user/user-header';
+import { FlowNavLink } from './flow-nav-link';
+
+type FlowNavProps = {
+  follow?: boolean;
+};
+
+const allNavs = [
+  [
+    { name:'Pay', src: '/main/ui/header/primary/pay/quick-pay.svg', path: 'pay' },
+    { name:'Wallet', src: '/main/ui/header/primary/pay/wallet.svg', path: 'pay/wallet' }
+
+ 
+  ]
+
+] as const;
+
+export function FlowNav({ follow }: FlowNavProps): JSX.Element {
+  const flowNav = allNavs[+!!follow];
+
+  return (
+    <motion.nav
+      className={cn(
+        `hover-animation flex justify-between overflow-y-auto
+         border-b border-light-border dark:border-dark-border`,
+        follow && 'mt-1 mb-0.5'
+      )}
+      {...variants}
+      exit={undefined}
+    >
+      {flowNav.map(({ name, src, path }) => (
+
+        <FlowNavLink src={src} name={name} path={path}  key={src} />
+      
+      ))}
+    </motion.nav>
+  );
+}
