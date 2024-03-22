503a1d5c74e678a6e69c698196d3c3216f85c2c4
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-23 00:05:51 +1200

    Update flow.tsx

--- src/pages/pay/flow.tsx
+++ src/pages/pay/flow.tsx
@@ -21,7 +21,7 @@ import { PaymentsButton } from '@components/pay/payments-button';
 import { WalletButton } from '@components/pay/wallet-button';
 import { PaymentCard } from '@components/pay/paymentcard';
 import { PaySettingsButton } from '@components/pay/pay-settings-button';
-import { PayNav } from '@components/navbar/primary/pay/pay-nav';
+import { FlowNav } from '@components/navbar/primary/pay/flow-nav';
 import type { ReactElement, ReactNode } from 'react';
 
 export default function Pay(): JSX.Element {
@@ -32,14 +32,14 @@ export default function Pay(): JSX.Element {
   
   return (
     <MainContainer>
-      <SEO title='Make Payment | Aria+' />
+      <SEO title='Pay | Aria+' />
       <MainHeader     useActionButton action={back} 
         title='Pay'
         className='flex items-center justify-between text-[0px]'
       > 
        <PaySettingsButton/>
       </MainHeader>
-      <PayNav/>
+      <FlowNav/>
       <div className='px-8'>
       <PaymentCard />
         </div>
