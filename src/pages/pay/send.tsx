212ca2438ae8b8a88405c0d7fac8212a4561f907
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-23 12:08:02 +1200

    Update send.tsx

--- src/pages/pay/send.tsx
+++ src/pages/pay/send.tsx
@@ -40,7 +40,7 @@ export default function Send(): JSX.Element {
        <PaySettingsButton/>
       </MainHeader>
       <FlowNav/>
-      <div className='px-8'>
+      <div className='self-center'>
       <PaymentCard />
         </div>
     </MainContainer>
