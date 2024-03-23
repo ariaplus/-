38f65c162993b6f0f26420cdd8fbb51f4082e245
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-23 14:06:01 +1200

    Update paymentcard.tsx

--- src/components/pay/paymentcard.tsx
+++ src/components/pay/paymentcard.tsx
@@ -11,7 +11,16 @@ const userId = user?.id as string;
     <Link href='/pay/flow'>
       <a className='block bg-cover border-solid border-2 border-white bg-center h-[13rem] w-[350px] xs:h-[13rem] sm:h-[13rem] sm:w-[350px] rounded-2xl shadow-md' style={{ backgroundImage: 'url(/assets/blue.jpg)' }}>
 
-          <div className='flex p-4 items:end content-end '>
+          <div className='flex p-4 items:end content-end justify-between'>
+                    <ArplImage
+          imgClassName='arplicon'
+          blurClassName='none'
+          src='/assets/explore.svg'
+          alt=''
+          layout='fill'
+          width='25px'
+          height='25px'
+       />
         <p className='pl-2 text-xl text-light-secondary dark:text-dark-secondary'>
             $0
           </p>
