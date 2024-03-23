bd4b7fde85ba5c41aa6f636b50f9afcdcc768dc7
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-24 10:03:10 +1200

    Create sports-nav.tsx

---
+++ src/components/navbar/primary/sports/sports-nav.tsx
@@ -0,0 +1,45 @@
+import { motion } from 'framer-motion';
+import cn from 'clsx';
+import { variants } from '@components/user/user-header';
+import { SportsNavLink } from './sports-nav-link';
+
+type SportsNavProps = {
+  follow?: boolean;
+};
+
+const allNavs = [
+  [
+    { name:'Soccer', src: '/main/ui/header/primary/sports/sports-filled.svg', path: 'sports' },
+    { name:'', src: '/main/ui/header/primary/sports/soccer.svg', path: 'soccer' },
+    { name:'', src: '/main/ui/header/primary/sports/football.svg', path: 'football' },
+    
+    {  name:'', src: '/main/ui/header/primary/sports/basketball.svg', path: 'basketball' },
+    { name:'', src: '/main/ui/header/primary/sports/baseball.svg', path: 'baseball' },
+    {  name:'', src: '/main/ui/header/primary/sports/tennis.svg', path: 'tennis' },
+    {  name:'', src: '/main/ui/header/primary/sports/golf.svg', path: 'golf' },
+    {  name:'', src: '/main/ui/header/primary/sports/esports2.svg', path: 'esports' }
+  ]
+
+] as const;
+
+export function SportsNav({ follow }: SportsNavProps): JSX.Element {
+  const sportsNav = allNavs[+!!follow];
+
+  return (
+    <motion.nav
+      className={cn(
+        `hover-animation flex justify-between overflow-y-auto
+         border-b border-light-border dark:border-dark-border`,
+        follow && 'mt-1 mb-0.5'
+      )}
+      {...variants}
+      exit={undefined}
+    >
+      {sportsNav.map(({ name, src, path }) => (
+
+        <SportsNavLink src={src} name={name} path={path}  key={src} />
+      
+      ))}
+    </motion.nav>
+  );
+}
