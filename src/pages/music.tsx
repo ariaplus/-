5a5df2033d371469babe946d40f68c35d4f1f72f
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-24 14:12:18 +1200

    Update music.tsx

--- src/pages/music.tsx
+++ src/pages/music.tsx
@@ -10,6 +10,8 @@ import { MainContainer } from '@components/home/main-container';
 import { AsideTrends } from '@components/aside/aside-trends';
 import { Button } from '@components/ui/button';
 import { ToolTip } from '@components/ui/tooltip';
+import { MusicNav  } from '@components/navbar/primary/music/music-nav';
+
 import { HeroIcon } from '@components/ui/hero-icon';
 import type { ReactElement, ReactNode } from 'react';
 import { useAuth } from '@lib/context/auth-context';
@@ -24,11 +26,10 @@ export default function Bookmarks(): JSX.Element {
   return (
     <MainContainer>
       <SEO title='Music | Aria+' />
-      <MainHeader useActionButton action={back}>
+      <MainHeader useActionButton action={back} className='justify-between' >
           <div className='-mb-1 flex flex-col'>
           <h2 className='-mt-1 text-xl font-bold'>Aria+ Music</h2>
-          <p className='text-xs text-light-secondary dark:text-dark-secondary'>
-            @{user?.username}
+     
           </p>
         </div>
         <Button
@@ -39,6 +40,7 @@ export default function Bookmarks(): JSX.Element {
           <ToolTip tip='Settings' />
         </Button>
       </MainHeader>
+    <MusicNav/>
   <NewMusicSlide/>
     </MainContainer>
   );
