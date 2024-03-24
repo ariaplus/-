111c1ebc129005bc7539ce0bcb8beae2c8c29417
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-24 12:59:57 +1200

    Create easter.tsx

---
+++ src/pages/easter.tsx
@@ -0,0 +1,74 @@
+import { AnimatePresence } from 'framer-motion';
+import { where, orderBy } from 'firebase/firestore';
+import { useWindow } from '@lib/context/window-context';
+import { useInfiniteScroll } from '@lib/hooks/useInfiniteScroll';
+import { tweetsCollection } from '@lib/firebase/collections';
+import { HomeLayout, ProtectedLayout } from '@components/layout/common-layout';
+import { MainLayout } from '@components/layout/main-layout';
+import { SEO } from '@components/common/seo';
+import { MainContainer } from '@components/home/main-container';
+import { UpdateUsername } from '@components/home/update-username';
+import { MainHeader } from '@components/home/main-header';
+import { Tweet } from '@components/tweet/tweet';
+import { useRouter } from 'next/router';
+
+import { ExploreNav } from '@components/navbar/primary/explore/explore-nav';
+import { Loading } from '@components/ui/loading';
+import { Error } from '@components/ui/error';
+import { DiscoverButton } from '@components/buttons/primary/discover-button';
+import { ExploreButton } from '@components/buttons/primary/explore-button';
+import { ServerDown } from '@components/maintenance/server-down';
+import type { ReactElement, ReactNode } from 'react';
+
+
+export default function Easter(): JSX.Element {
+  const { isMobile } = useWindow();
+  const { back } = useRouter();
+             
+  const { data, loading, LoadMore } = useInfiniteScroll(
+    tweetsCollection,
+    [where('parent', '==', null), orderBy('createdAt', 'desc')],
+    { includeUser: true, allowNull: true, preserve: true }
+  );
+
+  return (
+    <MainContainer className='no-scrollbar'>
+      <SEO title='Easter | Aria+' />
+      <MainHeader
+        useActionButton action={back} 
+        title='Easter'
+        className='flex items-center justify-between text-[0px]'
+      > <div className='flex items-right' >
+        <ExploreButton />
+        <DiscoverButton />
+         </div>
+      </MainHeader>
+      <ExploreNav/>
+      <ServerDown/>
+      <section className='mt-0.5 xs:mt-0 no-scrollbar'>
+        {loading ? (
+          <Loading className='mt-5' />
+        ) : !data ? (
+          <Error message='Something went wrong' />
+        ) : (
+          <>
+            <AnimatePresence mode='popLayout'>
+              {data.map((tweet) => (
+                <Tweet {...tweet} key={tweet.id} />
+              ))}
+            </AnimatePresence>
+            <LoadMore />
+          </>
+        )}
+      </section>
+    </MainContainer>
+  );
+}
+
+Easter.getLayout = (page: ReactElement): ReactNode => (
+  <ProtectedLayout>
+    <MainLayout>
+      <HomeLayout>{page}</HomeLayout>
+    </MainLayout>
+  </ProtectedLayout>
+);
