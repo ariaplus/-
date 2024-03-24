aa23d8e620e5b2d3548708b87650e028e6468bab
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-24 12:02:57 +1200

    Update business.tsx

--- src/pages/business.tsx
+++ src/pages/business.tsx
@@ -16,7 +16,6 @@ import { useRouter } from 'next/router';
 import { Tweet } from '@components/tweet/tweet';
 import { Loading } from '@components/ui/loading';
 import { Error } from '@components/ui/error';
-import { Start } from '@components/modal/discover/start';
 import { ExploreButton } from '@components/explore/header/explore-button';
 import { NewsButton } from '@components/explore/header/news-button';
 import type { ReactElement, ReactNode } from 'react';
