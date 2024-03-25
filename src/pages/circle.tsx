e949c7913580406d3687507ba1d2db5b098f0314
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-26 11:26:44 +1200

    Create circle.tsx

---
+++ src/pages/circle.tsx
@@ -0,0 +1,116 @@
+import { useMemo } from 'react';
+import { AnimatePresence } from 'framer-motion';
+import { toast } from 'react-hot-toast';
+import { orderBy, query } from 'firebase/firestore';
+import { useAuth } from '@lib/context/auth-context';
+import { useModal } from '@lib/hooks/useModal';
+import { useRouter } from 'next/router';
+import { useCollection } from '@lib/hooks/useCollection';
+import { useArrayDocument } from '@lib/hooks/useArrayDocument';
+import { clearAllBookmarks } from '@lib/firebase/utils';
+import {
+  tweetsCollection,
+  userBookmarksCollection
+} from '@lib/firebase/collections';
+import { MessagesLayout, ProtectedLayout } from '@components/layout/common-layout';
+import { MainLayout } from '@components/layout/main-layout';
+import { SEO } from '@components/common/seo';
+import { MainHeader } from '@components/home/main-header';
+import { MainContainer } from '@components/home/main-container';
+import { Modal } from '@components/modal/modal';
+import { ActionModal } from '@components/modal/action-modal';
+import { Tweet } from '@components/tweet/tweet';
+import { StatsEmpty } from '@components/tweet/stats-empty';
+import { Button } from '@components/ui/button';
+import { ToolTip } from '@components/ui/tooltip';
+import { HeroIcon } from '@components/ui/hero-icon';
+import { Loading } from '@components/ui/loading';
+import type { ReactElement, ReactNode } from 'react';
+
+export default function Circle(): JSX.Element {
+    const { back } = useRouter();
+  const { user } = useAuth();
+
+  const { open, openModal, closeModal } = useModal();
+
+  const userId = user?.id as string;
+
+  const { data: bookmarksRef, loading: bookmarksRefLoading } = useCollection(
+    query(userBookmarksCollection(userId), orderBy('createdAt', 'desc')),
+    { allowNull: true }
+  );
+
+  const tweetIds = useMemo(
+    () => bookmarksRef?.map(({ id }) => id) ?? [],
+    [bookmarksRef]
+  );
+
+  const { data: tweetData, loading: tweetLoading } = useArrayDocument(
+    tweetIds,
+    tweetsCollection,
+    { includeUser: true }
+  );
+
+  const handleClear = async (): Promise<void> => {
+    await clearAllBookmarks(userId);
+    closeModal();
+    toast.success('Successfully cleared all bookmarks');
+  };
+
+  return (
+    <MainContainer>
+      <SEO title='Circle | Aria+' />
+      <Modal
+        modalClassName='max-w-xs bg-main-background w-full p-8 rounded-2xl'
+        open={open}
+        closeModal={closeModal}
+      >
+        <ActionModal
+          title='Clear all Bookmarks?'
+          description='This can’t be undone and you’ll remove all Tweets you’ve added to your Bookmarks.'
+          mainBtnClassName='bg-accent-red hover:bg-accent-red/90 active:bg-accent-red/75 accent-tab 
+                            focus-visible:bg-accent-red/90'
+          mainBtnLabel='Clear'
+          action={handleClear}
+          closeModal={closeModal}
+        />
+      </Modal>
+      
+          <MainHeader useActionButton action={back} className='flex items-center justify-between '>
+        <div className='-mb-1 justify-between flex flex-col'>
+          <h2 className='-mt-1 text-xl font-bold'>Circle</h2>
+      
+        </div>
+        
+        <Button
+          className='dark-bg-tab group relative p-2 hover:bg-light-primary/10
+                     active:bg-light-primary/20 dark:hover:bg-dark-primary/10 
+                     dark:active:bg-dark-primary/20'
+          onClick={openModal}
+        >
+          <HeroIcon className='h-5 w-5' iconName='ArchiveBoxXMarkIcon' />
+          <ToolTip
+            className='!-translate-x-20 translate-y-3 md:-translate-x-1/2'
+            tip='Clear bookmarks'
+          />
+        </Button>
+      </MainHeader>
+      <section className='mt-0.5'>
+        <div className='demo'>
+         <div className='cortana-loader'></div>
+         </div>
+        <p className='text-base'>
+  I think we both know the answer to that.
+        </p>
+      </section>
+    </MainContainer>
+  );
+}
+
+Circle.getLayout = (page: ReactElement): ReactNode => (
+  <ProtectedLayout>
+    <MainLayout>
+      <MessagesLayout>{page}</MessagesLayout>
+    </MainLayout>
+  </ProtectedLayout>
+);
