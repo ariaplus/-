11f7c617131d677f15d255c48a93b1b0d13ca84d
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-26 19:06:30 +1200

    Create aside-transactions.tsx

---
+++ src/components/aside/primary/pay/aside-transactions.tsx
@@ -0,0 +1,47 @@
+import Link from 'next/link';
+import cn from 'clsx';
+import { motion } from 'framer-motion';
+import { formatNumber } from '@lib/date';
+import { preventBubbling } from '@lib/utils';
+import { useTrends } from '@lib/api/trends';
+import { Error } from '@components/ui/error';
+import { HeroIcon } from '@components/ui/hero-icon';
+import { Button } from '@components/ui/button';
+import { ToolTip } from '@components/ui/tooltip';
+import { Loading } from '@components/ui/loading';
+import type { MotionProps } from 'framer-motion';
+import { NextImage } from '@components/ui/next-image';
+import { RightInput } from '@components/input/right-input';
+import { useWindow } from '@lib/context/window-context';
+
+
+export const variants: MotionProps = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  transition: { duration: 0.8 }
+};
+
+type AsideTransactionsProps = {
+  inTrendsPage?: boolean;
+};
+
+export function AsideTransactions({ inTrendsPage }: AsideTransactionsProps): JSX.Element {
+  const { data, loading } = useTrends(1, inTrendsPage ? 100 : 10, {
+    refreshInterval: 30000
+  });
+
+  const { trends, location } = data ?? {};
+   const { isMobile } = useWindow();
+
+
+  return (
+    
+    <section
+      className='hover-animation rounded-2xl bg-black border border-slate-700'>
+    
+      <p className='text-base text-center '>
+      </p>
+
+    </section>
+  );
+}
