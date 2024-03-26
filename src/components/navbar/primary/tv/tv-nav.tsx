5a47de0397551dbd7ca6ac63c342491f749b758f
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-03-26 14:10:32 +1200

    Update tv-nav.tsx

--- src/components/navbar/primary/tv/tv-nav.tsx
+++ src/components/navbar/primary/tv/tv-nav.tsx
@@ -10,10 +10,10 @@ type TvNavProps = {
 const allNavs = [
   [
     { name:'tv', src: '/main/ui/primary/tv/3d-tv-box.svg', path: 'tv' },
-    { name:'Featured', src: '/main/ui/header/primary/fashion/makeup.svg', path: 'featured' },
+    { name:'Featured', src: '/main/ui/primary/tv/featured.svg', path: 'tv/featured' },
   { name:'Originals', src: '/main/ui/header/primary/fashion/fashion-clothing.svg', path: 'originals' },
-    { name:'Categories', src: '/main/ui/header/primary/fashion/fashionshow.svg', path: 'categories' },
-       { name:'Watchlist', src: '/main/ui/header/primary/fashion/down.svg', path: 'watchlist' }
+    { name:'Watchlist', src: '/main/ui/header/primary/fashion/fashionshow.svg', path: 'tv/watchlist' },
+       { name:'Remote', src: '/main/ui/primary/tv/tv-cat.svg', path: 't/remote' }
  
   ]
 
